056239afa04861af61a574c06b171f77beaee964
liujunli <liujunli@vipkid.com.cn>
2020-03-30 17:41:59 +0800

    changed address

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -16,7 +16,7 @@ module.exports = {
     themeConfig: {
         logo: '/logo.png',
         // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
-        repo: 'https://github.com/vipkid-edu/vf.js',
+        repo: 'https://github.com/vipkid-edu/',
         // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
         // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
         repoLabel: '查看源码',
